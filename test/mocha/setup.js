cb566b711bdb09dffa924e58f9b2f1510fb1a3c1
asamuzaK (Kazz) <kazz@asamuzak.jp>
2019-05-10 10:15:25 +0900

    Add webext-schema

--- test/mocha/setup.js
+++ test/mocha/setup.js
@@ -4,7 +4,9 @@
 
 "use strict";
 const {JSDOM} = require("jsdom");
-const browser = require("sinon-chrome/webextensions");
+const {Schema} = require("webext-schema");
+const Api = require("sinon-chrome/api");
+const sinon = require("sinon");
 
 /**
  * create jsdom
@@ -21,9 +23,42 @@ const createJsdom = () => {
 const {window} = createJsdom();
 const {document} = window;
 
-global.browser = browser;
+const schema = new Schema("central").arrange({name: "sinon-chrome"});
+const browser = new Api(schema).create();
+
+/* mock runtime.Port */
+class Port {
+  /**
+   * Create stubbed object
+   * @param {Object} opt - options
+   */
+  constructor(opt = {}) {
+    this.name = opt.name;
+    this.sender = opt.sender;
+    this.error = {
+      message: sinon.stub(),
+    };
+    this.onDisconnect = {
+      addListener: sinon.stub(),
+      removeListener: sinon.stub(),
+    };
+    this.onMessage = {
+      addListener: sinon.stub(),
+      removeListener: sinon.stub(),
+    };
+    this.disconnect = sinon.stub();
+    this.postMessage = sinon.stub();
+  }
+}
+
+browser.runtime.Port = Port;
+browser.runtime.connect.returns(new Port());
+browser.runtime.connectNative.callsFake(name => new Port({name}));
+browser.i18n.getMessage.callsFake((...args) => args.toString());
+
 global.window = window;
 global.document = document;
+global.browser = browser;
 
 module.exports = {
   browser,
